6d2ed8e4a844a8dd7951cb9597f24beae2d2548b
Tytianiuk Artem <tytianiuk@gmail.com>
2024-11-21 12:44:54 +0200

    fix:move сontacts page to modules directory
    - move сontacts page to modules directory
    - changed regular function to arrow function

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,16 +1,11 @@
-import ContactForm from '@/components/contacts/contactForm';
-import ContactInfo from '@/components/contacts/contactInfo';
+import ContactsPage from '@/modules/contacts/contacts-page';
 
-export default function ContactPage() {
+const Contacts = () => {
   return (
-    <div className="min-h-screen bg-white text-black">
-      <main className="container mx-auto px-4 py-12">
-        <h2 className="mb-8 text-2xl font-semibold">Зв&#39;яжіться з нами</h2>
-        <div className="grid gap-12 md:grid-cols-2">
-          <ContactForm />
-          <ContactInfo />
-        </div>
-      </main>
+    <div>
+      <ContactsPage />
     </div>
   );
-}
+};
+
+export default Contacts;
