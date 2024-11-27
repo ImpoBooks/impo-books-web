8335bcca486796301b050ccd61af0bde8cbe0c07
Tytianiuk Artem <tytianiuk@gmail.com>
2024-11-27 02:40:09 +0200

    feat: implement submit of form data - add roles to some tags for future testing

--- src/components/contacts/contactForm.tsx
+++ src/components/contacts/contactForm.tsx
@@ -1,22 +1,73 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
 import { Button } from '@/components/ui/button';
 import { formItems } from '@/constants/form-items';
+import { useToast } from '@/hooks/use-toast';
+import {
+  contactsFormSchema,
+  contactsFormValues,
+  FormValues,
+} from '@/modules/contacts/constants';
 
-const ContactForm = () => {
+const ContactForm: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FormValues>({
+    resolver: zodResolver(contactsFormSchema),
+    defaultValues: contactsFormValues,
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const { toast } = useToast();
+
+  const onSubmit: SubmitHandler<FormValues> = async () => {
+    setIsSubmitting(true);
+    try {
+      reset();
+    } catch {
+      toast({
+        title: 'Помилка при надісланні повідомлення',
+        description: 'Перевірте правильність введених даних',
+        variant: 'destructive',
+      });
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
-    <form className="space-y-6">
-      {formItems.map(({ id, label, component: Component, ...props }) => (
-        <div key={id}>
-          <label htmlFor={id} className="block mb-2 text-sm font-medium">
-            {label}
-          </label>
-          <Component id={id} {...props} />
-        </div>
-      ))}
+    <form className="space-y-6" role="form" onSubmit={handleSubmit(onSubmit)}>
+      {formItems.map(({ id, label, component: Component, ...props }) => {
+        return (
+          <div key={id}>
+            <label htmlFor={id} className="block mb-2 text-sm font-medium">
+              {label}
+            </label>
+            <Component
+              id={id}
+              {...register(id as keyof FormValues, {})}
+              {...props}
+            />
+            {errors[id as keyof FormValues] && (
+              <p className="mt-1 text-sm text-red-600">
+                {errors[id as keyof FormValues]?.message}
+              </p>
+            )}
+          </div>
+        );
+      })}
       <Button
         type="submit"
         className="w-full bg-black text-white hover:bg-gray-800"
+        disabled={isSubmitting}
       >
-        Відправити
+        {isSubmitting ? 'Відправляється...' : 'Відправити'}
       </Button>
     </form>
   );
