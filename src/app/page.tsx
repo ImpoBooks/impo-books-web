ddb6d90a12e8a2d9425ca4de5573319c21ef4ea2
Ihor Novykov <ihor.n@develops.today>
2024-12-02 23:48:48 +0200

    feat: add metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from 'next';
 import { FC } from 'react';
 
 import CatalogPage from '@/modules/catalog/catalog-page';
@@ -6,6 +7,10 @@ import { Filters } from '@/types/book';
 interface CatalogProps {
   searchParams: Filters;
 }
+
+export const metadata: Metadata = {
+  title: 'Каталог | ImpoBooks',
+};
 
 const Catalog: FC<CatalogProps> = async ({ searchParams }) => {
   return <CatalogPage filters={searchParams} />;
